baa777455f84e8667bc2a436b76f6902ef435287
LeonardoMazzoni <leonardomazzoni02@gmail.com>
2021-02-03 09:31:32 +0100

    aggiunto database.sql

---
+++ files/Database.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM testestpsit.studenti
+LIMIT 0, 1000
+
+-- Date: 2021-02-03 09:27
+*/
+INSERT INTO `` (`id`,`nome`,`cognome`) VALUES (1,'Leonardo','Mazzoni');
+INSERT INTO `` (`id`,`nome`,`cognome`) VALUES (2,'Andrea','Buono');
+INSERT INTO `` (`id`,`nome`,`cognome`) VALUES (3,'Andrea','Lotti');
+INSERT INTO `` (`id`,`nome`,`cognome`) VALUES (4,'Stefano','Caiazzo');
+INSERT INTO `` (`id`,`nome`,`cognome`) VALUES (5,'Francesco','Lazzarelli');
+INSERT INTO `` (`id`,`nome`,`cognome`) VALUES (6,'Lorenzo','Camigliano');
